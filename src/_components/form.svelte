9db3484c38d091232fd0411d118f7ede38c41565
igorovic <hello@dyve.dev>
2021-08-31 13:08:01 +0200

    initial

---
+++ src/_components/form.svelte
@@ -0,0 +1,6 @@
+<script>
+	let name = 'world';
+</script>
+
+<h1>Hello {name}!</h1>
+<h2>sub title</h2>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
+<script>
+  import Hello from "../_components/form.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Hello></Hello>
+
+<style lang="scss">
+    $color: red;
+  
+    h1 {
+      color: $color;
+    }
+  
+    div {
+      background: green;
+  
+      > p {
+        color: #fff;
+      }
+    }
+  </style>
+  
